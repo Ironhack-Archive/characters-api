c977ab1ed7b6d4b765603631d579f9f2e10575a0
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2017-01-24 10:38:38 -0500

    Fix jQuery complaining about HTTP verbs
    When you do AJAX for any verb but GET and POST, it sends a OPTIONS request first to confirm that they method can be done. If the OPTIONS response doesn't have a Access-Control-Allow-Methods header with the requested method in the list, the browser won't do the request.

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ function createServer (data) {
 
   app.use(function (req, res, next) {
       res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'DELETE,GET,HEAD,PATCH,PUT');
       res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
       next();
   });
